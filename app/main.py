5cb4975ac370c10a7eeb158504e28f87eac84eb8
EssisDez <desireeessis@gmail.com>
2026-09-12 15:40:41 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,47 @@
 # app/main.py
 
+from pathlib import Path
 import streamlit as st
 import joblib
-import numpy as np
 
-# Load vectorizer and model
-vectorizer = joblib.load('C:/Users/Mtechbro-94/Desktop/SMS_Spam_Detector/tfidf_vectorizer.pkl')
-model = joblib.load('C:/Users/Mtechbro-94/Desktop/SMS_Spam_Detector/svm_model.pkl')
+#Model and Vecrorizer Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+VECTORIZER_PATH = BASE_DIR / "tfidf_vectorizer.pkl"
+MODEL_PATH = BASE_DIR / "svm_model.pkl"
+
+vectorizer = joblib.load(VECTORIZER_PATH)
+model = joblib.load(MODEL_PATH)
 
 # App UI
+# Add a background image
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://www.pcworld.com/wp-content/uploads/2026/01/shutterstock_2495795811-2.jpg?quality=50&strip=all");
+        background-size: cover;
+        background-opacity: 0.4;
+        background-position: top center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# Add a Title and Description
 st.set_page_config(page_title="SMS Spam Classifier", layout="centered")
-st.title("📩 SMS Spam Detection using SVM")
+
+st.markdown(
+    "<h1 style='color: darkblue; text-align: center;'>📩 SMS Spam Detection System</h1>",
+    unsafe_allow_html=True,
+)
 
 st.markdown("Enter a message below to check if it's spam or not.")
-
 # Text input
-user_input = st.text_area("✍️ Enter SMS Text Here:")
+user_input = st.text_area("Enter SMS Text Here:")
 
-# Predict button
-if st.button("🔍 Predict"):
+# Predict Spam or Not
+if st.button("🔍Predict"):
     if user_input.strip() == "":
         st.warning("Please enter a message to classify.")
     else:
@@ -28,6 +51,6 @@ if st.button("🔍 Predict"):
         
         # Display result
         if prediction == "spam":
-            st.error("🚨 This message is SPAM!")
+            st.error("This message is SPAM! Please be cautious and avoid clicking on any links or providing personal information.")
         else:
-            st.success("✅ This message is NOT SPAM.")
+            st.success("This message is NOT SPAM and is safe to read.")
